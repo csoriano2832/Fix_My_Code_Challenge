76d10222cbf9dc524242de66f9ff21f000c5e6f4
csoriano2832 <2832@holbertonschool.com>
2021-09-12 12:45:28 +0000

    fix unittest cases

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- User Model
+User Model
 """
 import hashlib
 import uuid
@@ -86,11 +86,11 @@ if __name__ == '__main__':
 
     if user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
-password")
+                password")
 
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
-password")
+                password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -100,4 +100,4 @@ password")
 
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
-before")
+                        before")

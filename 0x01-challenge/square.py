6662e55fa31dc500a7193202e3b7bbe299290e98
csoriano2832 <2832@holbertonschool.com>
2021-10-05 16:18:46 +0000

    first commit

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module defines the following class:
+    Square()
+"""
+
+
+class Square():
+    """
+    This class defines some basic functions that you can do with a square
+    """
+    
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        """ Constructor method """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_of_my_square(self):
+        """ Perimeter of the square """
+        return (self.width * 4)
+
+    def __str__(self):
+        """ Output of object as a string """
+        return "{}/{}".format(self.width, self.width)
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
